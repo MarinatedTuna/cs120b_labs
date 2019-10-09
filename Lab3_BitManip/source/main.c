e10c0d370aa2c34865e6f83f1d4bbebfb250345e
Joseph Tibog <jtibo002@wch136-23.cs.ucr.edu>
2019-10-08 18:04:38 -0700

    Lab3 working hard af

--- Lab3_BitManip/source/main.c
+++ Lab3_BitManip/source/main.c
@@ -28,29 +28,30 @@ int main(void) {
 	if(PINA == 1 || PINA == 2){
 	    //PA0 or PA1 is 1
 	    //PC5 = 1;
-	    PINC = 0x00 | PC5;
+	    PINC = PC5;
 	    PORTC = PINC;
 	}
 	if(PINA == 3 || PINA == 4) {
 //	    store = PC5 | PC4; //experiment bitwise or
-	    PINC = 0x00 | (PC5 | PC4);
+	    PINC = (PC5 + PC4);
 	    PORTC = PINC;
 	}
 	if(PINA == 5 || PINA == 6) {
 //	    store = PC5 | PC4 | PC3;
-	    PINC = 0x00 | (PC5 + PC4 + PC3);
+//	    previously 0x00 | (PC5 | PC4 | PC3);
+	    PINC = (PC5 | PC4 | PC3);
 	    PORTC = PINC;	
 	}
 	if(PINA == 7 || PINA == 8 || PINA == 9) {
-	    PINC = 0x00 | (PC5 + PC4 + PC3 + PC2);
+	    PINC = (PC5 | PC4 | PC3 | PC2);
 	    PORTC = PINC;
 	}
 	if(PINA == 10 || PINA == 11 || PINA == 12) {
-	    PINC = 0x00 | (PC5 + PC4 + PC3 + PC2 + PC1);
+	    PINC = (PC5 | PC4 | PC3 | PC2 | PC1);
 	    PORTC = PINC;
 	}
 	if(PINA == 13 || PINA == 14 || PINA == 15) {
-	    PINC = 0x00 | (PC5 + PC4 + PC3 + PC2 + PC1 + PC0);
+	    PINC = (PC5 | PC4 | PC3 | PC2 | PC1 | PC0);
 	    PORTC = PINC;
 	}
     }
