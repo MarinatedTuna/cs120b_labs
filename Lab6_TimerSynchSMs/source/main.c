51e373335956761f2fe7a4a430b43bda8dbde5c2
Joseph Tibog <jtibo002@wch136-32.cs.ucr.edu>
2019-10-21 09:24:41 -0700

    That's a lot of new info brah

--- Lab6_TimerSynchSMs/source/main.c
+++ Lab6_TimerSynchSMs/source/main.c
@@ -1,16 +1,42 @@
 /*	Author: jtibo002
  *  Partner(s) Name: Jenaro Vega
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #6  Exercise #nah
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0;
+
+// Internal variables
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+    // AVR timer/counter controller register TCCR1
+    TCCR1B = 0x0B; // see notes
+
+    // AVR output compare register OCR1A
+    OCR1A = 125;
+
+    // Initialize avr counter
+    TCNT1 = 0;
+
+    _avr_timer_cntcurr = _avr_timer_M;
+    // TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+    // Enable global interrupts
+    SREG |= 0x80;
+}
+
 
 int main(void) {
     /* Insert DDR and PORT initializations */
