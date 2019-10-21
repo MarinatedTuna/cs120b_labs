7fe024009399423efa123039fef6d3129201ea59
Joseph Tibog <jtibo002@wch136-23.cs.ucr.edu>
2019-10-21 09:07:25 -0700

    Switch fiesta

--- Lab5_HardwareLightDisplay/source/main.c
+++ Lab5_HardwareLightDisplay/source/main.c
@@ -1,162 +1,44 @@
 /*	Author: jvega008
- *  Partner(s) Name: Joseph Tibog, Jenaro Vega
- *	Lab Section:
- *	Assignment: Lab #5  Exercise #2
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
+ *	 *  Partner(s) Name: Joseph Tibog, Jenaro Vega
+ *	  *	Lab Section:
+ *	   *	Assignment: Lab #  Exercise #
+ *	    *	Exercise Description: [optional - include for your own benefit]
+ *	     *
+ *	      *	I acknowledge all content contained herein, excluding template or example
+ *	       *	code, is my own original work.
+ *	        */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-
-enum States {init, press_sub, press_add, rel_sub, rel_add, wait, reset_p, reset_r} state;
-
-int sm_tick(int state){
-    unsigned char A0_add = PINA & 0x01;
-    unsigned char A1_sub = PINA & 0x02;
-    if(state == init){
-	PORTC = 7;
-    }   
-    switch(state){
-        case init:
-	    if(A1_sub && A0_add){
-		state = reset_p;
-		break;
-	    }
-	    else if(A0_add){
-	        state = press_add;
-		break;
-	    }
-	    else if(A1_sub){
-		state = press_sub;
-		break;
-	    }
-	    else{
-		state = init;
-		break;
-	    }
-	case press_sub:
-	     if(!A1_sub && !A0_add){
-		state = rel_sub;
-		break;
-	    }
-	    else if(A1_sub && A0_add){
-		state = reset_p;
-		break;
-	    }
-	    else{
-                state = press_sub;
-                break;
-            }
-	case press_add:
-             if(!A1_sub && !A0_add){
-                state = rel_add;
-                break;
-            }
-            else if(A1_sub && A0_add){
-                state = reset_p;
-                break;
-            }
-            else{
-                state = press_add;
-                break;
-            }
-	case rel_sub:
-	    state = wait;
-	    break;
-	case rel_add:
-	    state = wait;
-	    break;
-	case wait:
-	    if(A0_add && A1_sub){
-		state = reset_p;
-		break;
-	    }
-	    else if(A1_sub && (PORTC > 0)){
-		state = press_sub;
-		break;
-	    }
-	    else if(A0_add && (PORTC < 9)){
-		state = press_add;
-		break;
-	    }
-	    else{
-		state = wait;
-		break;
-	    }
-	case reset_p:
-	    if(A0_add && A1_sub){
-		state = reset_p;
-		break;
-	    }
-	    else{
-		state = reset_r;
-		break;
-	    }
-	case reset_r:
-	    if(A0_add && !A1_sub){
-		state = press_add;
-		break;
-	    }
-	    else if(A1_sub && !A0_add){
-		state = press_sub;
-		break;
-	    }
-	    else if(A1_sub && A0_add){
-		state = reset_p;
-		break;
-	    }
-	    else{
-		state = reset_r;
-		break;
-	    }
-	default:
-	    state = init;
-	    break;
-
-    }
-    switch(state){
-	case init:
-	    PORTC = 7; 
-	    break;
-	case press_add:
-	    break;
-	case press_sub:
-	    break;
-	case rel_add:
-	    PORTC = PORTC + 1;
-	    state = wait;
-	    break;
-	case rel_sub:
-	    PORTC = PORTC - 1;
-	    state = wait;
-	    break;
-	case wait: 
-	    break;
-	case reset_p:
-	    break;
-	case reset_r:
-	    PORTC = 0;
-	    break;
-    }
-
-return state;
-
-}
-
 int main(void) {
-    
     DDRA = 0x00; PORTA = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
-   
-    int state = init;
-    while (1) {
-	state = sm_tick(state);
-	
-    }
+
+	while (1) {
+		if(PINA == 0xFF){ //was 0xFF
+			PORTC = 0x40;
+		}
+		if(PINA == 0xFE || PINA == 0xFE){ //was 0xFE
+			PORTC = 0x60;
+		}
+		if(PINA == 0xFB || PINA == 0xFB){ //was 0xFB
+			PORTC = 0x70;
+		}
+		if(PINA == 0xFA || PINA == 0xF9){
+			PORTC = 0x38;
+		}
+		if(PINA == 0xF8 || PINA == 0xF7 || PINA == 0xF6){
+			PORTC = 0x3C;
+		}
+		if(PINA == 0xF5 || PINA == 0xF4 || PINA == 0xF3){
+			PORTC = 0x3E;
+		}
+		if(PINA == 0xF2 || PINA == 0XF1 || PINA == 0xF0){ //was 0xF2 || 0xF1 || 0xF0
+			PORTC = 0x3F;
+		}
+
+	}
     return 1;
 }
