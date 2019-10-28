77c7dbf47c5f1f081f90672ebbd184038594130f
Joseph Tibog <jtibo002@wch136-30.cs.ucr.edu>
2019-10-28 11:45:07 -0700

    grinding hard af baby

--- Lab7_LCDScreen/source/main.c
+++ Lab7_LCDScreen/source/main.c
@@ -1,61 +1,132 @@
-/*	Author: jtibo002
- *  Partner(s) Name: Jenaro Vega
+/*	Author: jvega008
+ *  Partner(s) Name: Joseph Tibog, Jenaro Vega
  *	Lab Section:
- *	Assignment: Lab #7  Exercise #1
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-#include <avr/io.h>
 #include "io.h"
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 volatile unsigned char TimerFlag = 0;
 
-void TimerISR() {
-    TimerFlag = 1;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+
+void TimerOn(){
+	TCCR1B = 0x0B;
+
+
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+
+	SREG |= 0x80;
 }
 
+void TimerOff(){
+	TCCR1B = 0x00;
+}
+
+void TimerISR(){
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+enum States {init,wait, press_sub, press_add, reset} state;
+unsigned char tmp = 0x00;
+void sm_tick(){
+	switch(state) {
+		case init:
+			state = wait;
+			break;
+		case wait:
+			if((~PINA & 0x03) == 0x03) {
+				state = reset;
+			}
+			else if ((~PINA & 0x01) == 0x01) {
+				state = press_add;
+			}
+			else if ((~PINA & 0x02) == 0x02) {
+				state = press_sub;
+			}
+			break;
+		case press_add:
+			state = wait;
+			break;
+		case press_sub:
+			state = wait;
+			break;
+		case reset: 
+			state = wait;
+			break;
+		default:
+			state = init;
+			break;
+		}
+		switch(state) {
+			case init: 
+				break;
+			case wait:
+				break;
+			case press_add:
+				if(tmp < 9) {
+					tmp++;
+				}
+				break;
+			case press_sub:
+			    if(tmp > 0) {
+					tmp--;
+				}
+				break;
+			case reset:
+				tmp = 0x00;
+				break;
+			default:
+				break;
+         }
+}
+
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF; // inputs
-
-    DDRC = 0xFF; PORTC = 0x00; // LCD data lines
-    DDRD = 0xFF; PORTD = 0x00; // LCD control lines
-
-    // Initializes the LCD display
-    LCD_init();
-
-    // Starting at position 1 on the LCD screen, writes "Hello World"
-//    LCD_DisplayString(1, "Hello World");
-
-    unsigned char num = 0;
-
-    //need timer
-    TimerSet(1000);
+    
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+    TimerSet(50);
     TimerOn();
 
-    /* Insert your solution below */
+    state = init;
+    tmp = 0x00;
+    LCD_init();
+    
     while (1) {
-	
-	while(!TimerFlag){}
-	    TimerFlag = 0;
-
-	if(PINA == 0x01) {
-            num = num + 1;
-	    LCD_WriteData(num + '0');
-	}
-	else if(PINA == 0x02) {
-	    num = num - 1;
-	    LCD_WriteData(num + '0');
-	}
-	else if(PINA == 0xFE || PINA == 0xFD){
-	    num = 0;
-	    LCD_WriteData(0 + '0');
-	}	
+		sm_tick();
+		LCD_Cursor(1);
+		LCD_WriteData(tmp + '0');	
+		while(!TimerFlag){
+		}
+		TimerFlag = 0;
     }
     return 1;
 }
