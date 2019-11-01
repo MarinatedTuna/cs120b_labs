6bd79da7e43aafd2473fd8f3f11e15227c7a01c6
Joseph Tibog <jtibo002@wch136-23.cs.ucr.edu>
2019-11-01 11:01:21 -0700

    asjkdfvb

--- Lab9_PWM/source/main.c
+++ Lab9_PWM/source/main.c
@@ -15,13 +15,28 @@
 void set_PWM(double frequency) {
     static double current_frequency;
     
-    if(frequency != current_frequency) {
-   //     TCRR3B &= 0x08;
-    }
-    else {
-   //     TCRR3B |= 0x03;
-    }
+    if (frequency != current_frequency) {
+        
+        if (!frequency) {
+            TCRR3B &= 0x08;
+        }
+        else {
+            TCRR3B |= 0x03;
+        }
 
+        if (freqency < 0.954) {
+            OCR3A = 0xFFFF;
+        }
+        else if (frequency > 31250) {
+            OCR3A = 0x0000;
+        }
+        else {
+            OCR3A = (short) (8000000 / (128 * frequency)) - 1;
+        }
+
+        TCNT3 = 0;
+        current_frequency = frequency;
+    }
 } 
 
 int main(void) {
