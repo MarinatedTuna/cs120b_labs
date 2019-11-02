da30095f4ecdb701870e57461e9256190e398fb0
Joseph Tibog <jtibo002@wch136-23.cs.ucr.edu>
2019-11-02 12:13:45 -0700

    Let's win it

---
+++ Lab10_ConcurrentSynchSMs/source/main.c
@@ -0,0 +1,25 @@
+/*	Author: jtibo002
+ *  Partner(s) Name: Jenaro Vega
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+// There are going to be 3 state machines
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
