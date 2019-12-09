3ed6ee41dbae52f2f0541ce68488921a0397ea92
Joseph Tibog <jtibo002@ucr.edu>
2019-12-08 16:21:36 -0800

    Create adc.h

---
+++ cs120b_final_project/adc.h
@@ -0,0 +1,29 @@
+#ifndef adc_h
+#define adc_h
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <string.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdint.h>
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+unsigned short ReadADC(unsigned char ch){
+	ch = ch & 0x07;
+	ADMUX |= ch;
+	
+	ADCSRA |= (1<<ADSC);
+	
+	while(!(ADCSRA & (1<<ADIF)));
+	
+	ADCSRA |= (1<<ADIF); //adc(1)
+	
+	return(ADC);
+}
+
+#endif
